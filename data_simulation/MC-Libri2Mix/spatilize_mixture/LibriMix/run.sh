0e431a28676e9bfa961ade6ca21913d769387048
Author Name <nanahou@scsegc.cm.cluster>
2021-10-15 16:16:18 +0800

    test

--- data_simulation/MC-Libri2Mix/spatilize_mixture/LibriMix/run.sh
+++ data_simulation/MC-Libri2Mix/spatilize_mixture/LibriMix/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 set -eu
-./generate_librimix.sh /export/home/nanahou/gm/demo/espnet_ss/espnet/egs2/wsj0_2mix_spatialized/gen_data/spatilize_mixture
+# generate the RIRs information via pyroomacoustics
+python run_sample_reverb_libri.py
+# generate MC-Libri2Mix dataset using generated RIRs
+./generate_librimix.sh /export/home/nanahou/gm/demo/espnet_ss/espnet/egs2/wsj0_2mix_spatialized/L-SpEx/data_simulation/MC-Libri2Mix/spatilize_mixture
